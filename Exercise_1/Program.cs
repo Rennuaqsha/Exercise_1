0f9851e4d231354470023333972bee9ca5fa3cda
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-10 17:00:45 +0800

    Add project files.

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿class program
+{
+    //Mendeklarasikan array int dengan ukuran 65
+    private int[] RAS = new int[65];
+
+    //deklarasi variabel int untuk menyimpan banyaknya data yang disimpan 
+    private int n;
+
+    public void read()
+    {
+        while (true)
+        {
+            Console.Write("masukkan banyaknya elemen pada arra: ");
+            string s = Console.ReadLine();
+            n = Int32.Parse(s);
+            if (n <= 65)
+                break;
+            else
+                Console.WriteLine("\nArray dapat mempunyai maksimal 65 elemen. \n");
+
+        }
+    }
+}
