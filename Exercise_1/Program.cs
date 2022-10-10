a14eb41ef7b5b6a9b883118f2a6e7a5ef2bc8ad1
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-10 17:22:24 +0800

    create class Program

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -1,13 +1,15 @@
 ﻿class program
 {
     //Mendeklarasikan array int dengan ukuran 65
-    private int[] RAS = new int[65];
+    private int[] Rennu = new int[65];
 
     //deklarasi variabel int untuk menyimpan banyaknya data yang disimpan 
     private int n;
 
+    //fungsi atau method untuk menerima masukkan 
     public void read()
     {
+        //menerima angka untuk menentukan banyaknya data yang disimpan pada array
         while (true)
         {
             Console.Write("masukkan banyaknya elemen pada arra: ");
@@ -19,5 +21,18 @@
                 Console.WriteLine("\nArray dapat mempunyai maksimal 65 elemen. \n");
 
         }
+        Console.WriteLine("");
+        Console.WriteLine("-----------------------");
+        Console.WriteLine(" masukkan elemen array ");
+        Console.WriteLine("-----------------------");
+
+        //pengguna memasukkan elemen pada array
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("<" + (i + 1) + ">");
+            string s1 = Console.ReadLine();
+            Rennu[i] = Int32.Parse(s1);
+        }
     }
+
 }
